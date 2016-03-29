b913a9a57a5610a909b9e1dcdd7bac67f180600c
Jeena Lee <ijeenalee@gmail.com>
2016-03-29 16:22:23 -0700

    Add scala directory

---
+++ scala/nth_Fibonacci.scala
@@ -0,0 +1,11 @@
+def fib(n: Int): BigInt = {
+  def fibHelper(n:Int, prev: BigInt = 0, next: BigInt = 1): BigInt = n match {
+    case 0 => prev
+    case 1 => next
+      // If n is not 0 or 1, next becomes prev and next+prev becomes
+      // next.  When n reaches 1, fibHelper will return next, which
+      // will be the sum of the two previous Fibonacci numbers.
+    case _ => fibHelper( n-1, next, (next + prev) )
+  }
+  fibHelper(n)
+}
