9a32834500d20e6a91e9c6a4f27b46e33ec667e2
Jeena Lee <ijeenalee@gmail.com>
2016-03-29 16:30:15 -0700

    Add another way to implement

--- scala/nth_Fibonacci.scala
+++ scala/nth_Fibonacci.scala
@@ -5,7 +5,17 @@ def fib(n: Int): BigInt = {
       // If n is not 0 or 1, next becomes prev and next+prev becomes
       // next.  When n reaches 1, fibHelper will return next, which
       // will be the sum of the two previous Fibonacci numbers.
-    case _ => fibHelper( n-1, next, (next + prev) )
+    case _ => fibHelper(n - 1, next, (next + prev))
+  }
+  fibHelper(n)
+}
+
+// Another way to do it without pattern matching is:
+def fib(n: Int): BigInt = {
+  def fibHelper(n: Int, prev: BigInt = 0, next: BigInt = 1): BigInt = {
+    if (n == 0) prev
+    else if (n == 1) next
+    else fibHelper(n - 1, next, (next + prev))
   }
   fibHelper(n)
 }
