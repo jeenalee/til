bbf5aa93fadb0cacc7bd362dd67bb922ab8ae073
Jeena Lee <ijeenalee@gmail.com>
2016-03-30 15:18:39 -0700

    Fix typo

--- scala/is_sorted.scala
+++ scala/is_sorted.scala
@@ -1,8 +1,8 @@
-def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+def isSorted[A](as: Array[A], f: (A,A) => Boolean): Boolean = {
   @annotation.tailred
   def loop(n: Int): Boolean =
     if (n+1 == as.length) true 
-    else if ((ordered(as(n), as(n+1))) == false) false
+    else if ((f(as(n), as(n+1))) == false) false
     else loop(n+1)
 
   loop(0)
@@ -12,3 +12,4 @@ def ordered(n: Int, m: Int): Boolean = {
   n <= m
 }
 
+isSorted(Array(1, 2, 3), ordered)
